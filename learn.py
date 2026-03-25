beac8e67831100d38f6ce53e6af34ee55c14e02d
alban linux fixe <alban.schmitt@hotmail.com>
2026-03-25 08:49:45 +0100

    mandatory fini commence bonus

--- learn.py
+++ learn.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def load_data(filename):
     kms = []
@@ -34,18 +35,37 @@ def train(kms, prices, learning_rate=0.1, iterations=10000):
     m = len(kms)
 
     for _ in range(iterations):
-        tmp_θ0 = sum((theta0 + theta1 * kms[i]) - prices[i] for i in range(m))
-        # 2. calculer la somme des erreurs × km pour tmp_θ1
-        # 3. mettre à jour theta0 et theta1 simultanément
+        sum_errors = 0
+        sum_errors_km = 0
+
+        for i in range(m):
+            sum_errors += (theta0 + theta1 * kms[i]) - prices[i] # sum_errors = sum((theta0 + theta1 * kms[i]) - prices[i] for i in range(m))
+            sum_errors_km += ((theta0 + theta1 * kms[i]) - prices[i]) * kms[i] # sum_errors_km = sum(((theta0 + theta1 * kms[i]) - prices[i]) * kms[i] for i in range(m))
+        theta0, theta1 = theta0 - learning_rate * (1/m) * sum_errors, theta1 - learning_rate * (1/m) * sum_errors_km
 
     return theta0, theta1
+
+def denormalize_thetas(theta0, theta1, km_min, km_max, price_min, price_max):
+    theta1_real = theta1 * (price_max - price_min) / (km_max - km_min)
+    theta0_real = theta0 * (price_max - price_min) + price_min - theta1_real * km_min
+    return theta0_real, theta1_real
 
 def main():
     kms, prices = load_data("data.csv")
     kms_norm, km_min, km_max = normalize(kms)
     prices_norm, price_min, price_max = normalize(prices)
-    print(kms)
-    print(kms_norm)
+
+    theta0, theta1 = train(kms_norm, prices_norm)
+    theta0, theta1 = denormalize_thetas(theta0, theta1, km_min, km_max, price_min, price_max)
+    print(f"Entraînement terminé. theta0={theta0:.2f}, theta1={theta1:.2f}")
+
+    try:
+        with open("thetas.json", "w") as f:
+            json.dump({"theta0": theta0, "theta1": theta1}, f)
+    except Exception as e:
+        print(f"Probleme avec le thetas.json : {e}")
+        exit(1)
+
 
 if __name__ == "__main__":
     main()
